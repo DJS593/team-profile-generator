12007f23fa629ae8083048fc4bfa3232b56a8f08
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-05-13 10:55:28 -0700

    adding test questions for the Employee object or class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,22 @@
-const checkIfEqual = require('../lib/Employee.js');
+const Employee = require('../lib/Employee.js');
 
-test('checks if 10 is equal to 10', () => {
-  expect(checkIfEqual(10, 10)).toBe(true);
+
+// test for the Employee object
+test ('creates an Employee object', () => {
+  const employee = new Employee('David', 1);
+
+  expect(employee.name).toBe('David');
+  expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toEqual(expect.any(String));
+});
+
+
+// test for getName()
+
+test ("get employee's name", () => {
+  const employee = new Employee('David');
+
+  expect(employee.getName()).toHaveProperty(name);
+  // not sure I need to add the 'name' property 
+
 });
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 var inquirer = require('inquirer');
+
+
+const promptUser = () => { 
+  return inquirer.prompt([
+  
+    // list of questions about each position
+    // all questions require an answer
+    {
+      // project title
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project? (Required)',
+      validate: titleInput => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log('Please enter the project title!');
+        }
+      }
+    },    
