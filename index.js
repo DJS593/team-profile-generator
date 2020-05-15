3c2e523ba0681451d6b7b3f13f9e530c2c208b31
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-05-15 14:19:07 -0700

    user questions added

--- index.js
+++ index.js
@@ -59,7 +59,127 @@ const promptUser = () => {
           console.log("Please enter the manager's office number!");
         }
       }
-    }
+    },
+    // need a way to ask if they want more team members or done, then move to the appropriate question
+    {
+      // user to decide if they want to include more team members or finish
+      type: 'rawlist',
+      name: 'add team',
+      message:  'Do you wish to add more team members?',
+      choices: ["Yes, add an engineer.", "Yes, add an intern.", "No, I am done."]
+
+
+
+
+    },
+    
+    
+    {
+      // engineer's name
+      type: 'input',
+      name: 'engineer name',
+      message: "What is the engineer's name? (Required)",
+      validate: engineerName => {
+        if (engineerName) {
+          return true;
+        } else {
+          console.log("Please enter the engineer's name!");
+        }
+      }
+    },
+    {
+      // engineer's ID
+      type: 'input',
+      name: 'engineer ID',
+      message: "What is the engineer's ID? (Required)",
+      validate: engineerId => {
+        if (engineerId) {
+          return true;
+        } else {
+          console.log("Please enter the engineer's ID!");
+        }
+      }
+    },      
+    {
+      // engineer's email
+      type: 'input',
+      name: 'engineer email',
+      message: "What is the engineer's email? (Required)",
+      validate: engineerEmail => {
+        if (engineerEmail) {
+          return true;
+        } else {
+          console.log("Please enter the engineer's email!");
+        }
+      }
+    },      
+    {
+      // engineer's GitHub account
+      type: 'input',
+      name: 'engineer GitHub',
+      message: "What is the engineer's GitHub account? (Required)",
+      validate: engineerGithub => {
+        if (engineerGithub) {
+          return true;
+        } else {
+          console.log("Please enter the engineer's GitHub account!");
+        }
+      }
+    },      
+    {
+      // intern's name
+      type: 'input',
+      name: 'intern name',
+      message: "What is the intern's name? (Required)",
+      validate: internName => {
+        if (internName) {
+          return true;
+        } else {
+          console.log("Please enter the intern's name!");
+        }
+      }
+    },
+    {
+      // intern's ID
+      type: 'input',
+      name: 'intern ID',
+      message: "What is the intern's ID? (Required)",
+      validate: internId => {
+        if (internId) {
+          return true;
+        } else {
+          console.log("Please enter the intern's ID!");
+        }
+      }
+    },      
+    {
+      // intern's email
+      type: 'input',
+      name: 'intern email',
+      message: "What is the engineer's email? (Required)",
+      validate: internEmail => {
+        if (internEmail) {
+          return true;
+        } else {
+          console.log("Please enter the intern's email!");
+        }
+      }
+    },      
+    {
+      // intern's university
+      type: 'input',
+      name: 'university',
+      message: "What is the intern's university? (Required)",
+      validate: internUniversity => {
+        if (internUniversity) {
+          return true;
+        } else {
+          console.log("Please enter the intern's university!");
+        }
+      }
+    },      
+  
+  
   ])
 };
 
